ccf882114808a51438c3b9f4d9954dfd6ee0c4b9
Pedro <pedromatu29@gmail.com>
2019-04-24 13:33:01 -0400

    prueba_apis

--- prueba.rb
+++ prueba.rb
@@ -3,8 +3,8 @@ require 'net/http'
 require 'json'
 require 'openssl'
 
-def request(add, api_key)
-    url = URI(add+"&api_key="+api_key)
+def request(address, api_key)
+    url = URI(address+"&api_key="+api_key)
     http = Net::HTTP.new(url.host, url.port)
 
     http.use_ssl = true
@@ -19,37 +19,19 @@ def request(add, api_key)
 end
 
 def build_web_page(data)
-    html = 
-    "<html>
-    <head>
-    <title>API Nasa</title>
-    <h1>Los de la nasa<h1>
-    </head>
-    <body>
-    <ul>"
+
+    web = "<ul>"
     data.each do |photo|
-        html += 
-        "<li>
-        <img src='" + photo["img_src"] + "'></li>"
+        web += "<li><img src=\"#{photo['img_src']}\">\n</li>"
     end
-    html += 
-    "</ul>
-    </body>
-    </html>"
+    web += "</ul>"
 
-    File.write('index.html', html)
+    File.write('index.html', web)
 end
 
-def photos_count(data)
-    counts = Hash.new
-    data.each do |x| 
-        counts[x["camera"]["name"]] = 1 + counts[x["camera"]["name"]].to_i
-    end
-    return counts
-end
+
 
 
 data = request("https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000", "2y4Kfc1QTMpM5TYq4VkPn0Fd652aJpbp516DxTDD")
 data = data["photos"][0..5]
 build_web_page(data)
-print photos_count(data)
